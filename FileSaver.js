4bc1fe7ec6db97489a37965dd13c12ca63b095b0
chenlingjie <chenlingjie@cd121.com>
2017-12-27 09:58:11 +0800

    xg

--- FileSaver.js
+++ FileSaver.js
@@ -16,21 +16,27 @@
 var saveAs = saveAs || (function(view) {
 	"use strict";
 	// IE <10 is explicitly unsupported
+		//TODO 检测浏览器，ie10以上
 	if (typeof view === "undefined" || typeof navigator !== "undefined" && /MSIE [1-9]\./.test(navigator.userAgent)) {
 		return;
 	}
 	var
 		  doc = view.document
 		  // only get URL when necessary in case Blob.js hasn't overridden it yet
+		//TODO 得到url
 		, get_URL = function() {
 			return view.URL || view.webkitURL || view;
 		}
+		//TODO 创建a标签
 		, save_link = doc.createElementNS("http://www.w3.org/1999/xhtml", "a")
+		//TODO 检查是否有下载属性
 		, can_use_save_link = "download" in save_link
+		//TODO 手动触发事件
 		, click = function(node) {
 			var event = new MouseEvent("click");
 			node.dispatchEvent(event);
 		}
+		//TODO 检测浏览器
 		, is_safari = /constructor/i.test(view.HTMLElement) || view.safari
 		, is_chrome_ios =/CriOS\/[\d]+/.test(navigator.userAgent)
 		, throw_outside = function(ex) {
@@ -41,6 +47,7 @@ var saveAs = saveAs || (function(view) {
 		, force_saveable_type = "application/octet-stream"
 		// the Blob API is fundamentally broken as there is no "downloadfinished" event to subscribe to
 		, arbitrary_revoke_timeout = 1000 * 40 // in ms
+		//TODO 超时
 		, revoke = function(file) {
 			var revoker = function() {
 				if (typeof file === "string") { // file is an object URL
@@ -51,6 +58,7 @@ var saveAs = saveAs || (function(view) {
 			};
 			setTimeout(revoker, arbitrary_revoke_timeout);
 		}
+		//TODO 触发主动触发filesaver的事件
 		, dispatch = function(filesaver, event_types, event) {
 			event_types = [].concat(event_types);
 			var i = event_types.length;
@@ -65,6 +73,7 @@ var saveAs = saveAs || (function(view) {
 				}
 			}
 		}
+		//TODO 格式化blob
 		, auto_bom = function(blob) {
 			// prepend BOM for UTF-8 XML and text/* types (including HTML)
 			// note: your browser will automatically convert UTF-16 U+FEFF to EF BB BF
@@ -74,6 +83,7 @@ var saveAs = saveAs || (function(view) {
 			return blob;
 		}
 		, FileSaver = function(blob, name, no_auto_bom) {
+		    //TODO 格式化blob
 			if (!no_auto_bom) {
 				blob = auto_bom(blob);
 			}
@@ -83,26 +93,32 @@ var saveAs = saveAs || (function(view) {
 				, type = blob.type
 				, force = type === force_saveable_type
 				, object_url
+				//TODO 触发各个生命hook
 				, dispatch_all = function() {
 					dispatch(filesaver, "writestart progress write writeend".split(" "));
 				}
 				// on any filesys errors revert to saving with object URLs
+				//TODO 不能通过a标签下载才会执行
 				, fs_error = function() {
+                    //TODO 如果有FileReader，就用FileReader
 					if ((is_chrome_ios || (force && is_safari)) && view.FileReader) {
 						// Safari doesn't allow downloading of blob urls
 						var reader = new FileReader();
 						reader.onloadend = function() {
 							var url = is_chrome_ios ? reader.result : reader.result.replace(/^data:[^;]*;/, 'data:attachment/file;');
+							//TODO 通过打开链接来实现下载
 							var popup = view.open(url, '_blank');
 							if(!popup) view.location.href = url;
 							url=undefined; // release reference before dispatching
 							filesaver.readyState = filesaver.DONE;
 							dispatch_all();
 						};
+						//TODO 以dataUrl的方式读取
 						reader.readAsDataURL(blob);
 						filesaver.readyState = filesaver.INIT;
 						return;
 					}
+					//TODO 没有fileReader，那就通过窗口来下载
 					// don't create more object URLs than needed
 					if (!object_url) {
 						object_url = get_URL().createObjectURL(blob);
@@ -122,14 +138,20 @@ var saveAs = saveAs || (function(view) {
 				}
 			;
 			filesaver.readyState = filesaver.INIT;
-
+			//TODO 可以通过a标签下载
 			if (can_use_save_link) {
+				//TODO 创建可以下载url
 				object_url = get_URL().createObjectURL(blob);
+				//TODO 触发点击a标签下载
 				setTimeout(function() {
 					save_link.href = object_url;
+					//TODO download的参数是文件名
 					save_link.download = name;
+					//TODO 触发
 					click(save_link);
+					//TODO 执行周期
 					dispatch_all();
+					//TODO 超时
 					revoke(object_url);
 					filesaver.readyState = filesaver.DONE;
 				});
@@ -139,11 +161,13 @@ var saveAs = saveAs || (function(view) {
 			fs_error();
 		}
 		, FS_proto = FileSaver.prototype
+		//TODO 入口的api
 		, saveAs = function(blob, name, no_auto_bom) {
 			return new FileSaver(blob, name || blob.name || "download", no_auto_bom);
 		}
 	;
 	// IE 10+ (native saveAs)
+		//TODO 如果是ie10+，有自己的save函数
 	if (typeof navigator !== "undefined" && navigator.msSaveOrOpenBlob) {
 		return function(blob, name, no_auto_bom) {
 			name = name || blob.name || "download";
@@ -151,6 +175,7 @@ var saveAs = saveAs || (function(view) {
 			if (!no_auto_bom) {
 				blob = auto_bom(blob);
 			}
+			//TODO 核心的api
 			return navigator.msSaveOrOpenBlob(blob, name);
 		};
 	}
@@ -178,7 +203,7 @@ var saveAs = saveAs || (function(view) {
 // `self` is undefined in Firefox for Android content script context
 // while `this` is nsIContentFrameMessageManager
 // with an attribute `content` that corresponds to the window
-
+//TODO 兼容规范
 if (typeof module !== "undefined" && module.exports) {
   module.exports.saveAs = saveAs;
 } else if ((typeof define !== "undefined" && define !== null) && (define.amd !== null)) {
